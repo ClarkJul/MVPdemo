f084ae2c5e9353425cfcd51fad026537e39b77cb
Clark <393417438@qq.com>
2019-08-17 19:57:18 +0800

    the first commit

---
+++ app/src/main/java/com/clark/mvpdemo/fragment/BooksFragment.java
@@ -0,0 +1,25 @@
+package com.clark.mvpdemo.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.clark.mvpdemo.R;
+
+public class BooksFragment extends Fragment {
+    public BooksFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_books, container, false);
+    }
+}
